d02612cbacb57b7e760ea83937d823cb04c7ea73
lolindirfaelivrin <fabrizio.nic@gmail.com>
2022-07-21 00:32:41 +0200

    Create Controller.php

---
+++ app/lib/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+class Controller {
+
+  public function model($model) {
+    require_once '../app/models/'. $model . '.php';
+
+    return new $model;
+  }
+
+  public function view($views, $data = []) {
+    if(file_exists('../app/views/'. $views . '.php')) {
+      require_once '../app/views/'. $views . '.php';
+    }
+    else {
+      die("Vista non esiste");
+    }
+  }
+}
+ ?>
