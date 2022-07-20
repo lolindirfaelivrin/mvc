ffbe22311402ba4c4ee2b97fc0161494711203e4
lolindirfaelivrin <fabrizio.nic@gmail.com>
2022-07-21 00:31:52 +0200

    Create require.php

---
+++ app/require.php
@@ -0,0 +1,19 @@
+<?php
+
+//Importa le librerie necessarie
+
+require_once 'lib/Controller.php';
+require_once 'lib/Core.php';
+require_once 'lib/Database.php';
+require_once 'config/config.php';
+
+require_once 'helpers/session.php';
+require_once 'helpers/mail.class.php';
+require_once 'helpers/template.class.php';
+
+//Inizializzo l'applicazione
+
+$init = new Core();
+
+
+ ?>
