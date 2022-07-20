a27e06a2eed90b269edc6687c1ecfe1e5d61c5db
lolindirfaelivrin <fabrizio.nic@gmail.com>
2022-07-21 01:03:47 +0200

    Create about.php

---
+++ app/views/pagina/about.php
@@ -0,0 +1,10 @@
+<?php require APPROOT.'/views/includes/head.php'; ?>
+
+ <div class="" id="section__landing">
+   <?php require APPROOT.'/views/includes/navigation.php'; ?>
+ </div>
+
+ <h1>About</h1>
+
+
+<?php require APPROOT.'/views/includes/footer.php'; ?>
