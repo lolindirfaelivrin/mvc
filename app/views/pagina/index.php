0e5b3821b2c598ded6ad8acd9fedd538fe4070f4
lolindirfaelivrin <fabrizio.nic@gmail.com>
2022-07-21 01:03:22 +0200

    Create index.php

---
+++ app/views/pagina/index.php
@@ -0,0 +1,10 @@
+<?php require APPROOT.'/views/includes/head.php'; ?>
+
+ <div class="" id="section__landing">
+   <?php require APPROOT.'/views/includes/navigation.php'; ?>
+ </div>
+
+ <h1>Home Page</h1>
+
+
+<?php require APPROOT.'/views/includes/footer.php'; ?>
