f7a03727c9f33f5f7088b059113118d742751ca0
lolindirfaelivrin <fabrizio.nic@gmail.com>
2022-07-21 00:36:23 +0200

    Create head.php

---
+++ app/views/includes/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="expires" content="-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $data['title']; ?></title>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/stile.css">
+  </head>
+  <body>
